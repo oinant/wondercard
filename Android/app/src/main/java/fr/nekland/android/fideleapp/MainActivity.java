2037d82609cce614377e0e9e953ef4b1946f0315
Nek (Maxime Veber) <nek.dev@gmail.com>
2015-11-28 11:17:54 +0100

    Removed test text

--- Android/app/src/main/java/fr/nekland/android/fideleapp/MainActivity.java
+++ Android/app/src/main/java/fr/nekland/android/fideleapp/MainActivity.java
@@ -121,8 +121,6 @@ public class MainActivity extends AppCompatActivity
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                text.setText("Haha");
             }
 
             @Override
